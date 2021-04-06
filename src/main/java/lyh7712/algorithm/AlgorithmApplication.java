ee485bd2457dab178ba9f7b18dbcb668486e30fd
lyh7712 <mmj2rv9@kakao.com>
2021-04-06 19:35:09 +0900

    Initial commit

---
+++ src/main/java/lyh7712/algorithm/AlgorithmApplication.java
@@ -0,0 +1,13 @@
+package lyh7712.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmApplication.class, args);
+	}
+
+}
