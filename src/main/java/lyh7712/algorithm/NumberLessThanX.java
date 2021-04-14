5eef17ec55e76819b3307352d621a1c3a5f0531c
lyh7712 <mmj2rv9@kakao.com>
2021-04-14 23:51:27 +0900

    백준 알고리즘: X보다 작은 수
    정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.

---
+++ src/main/java/lyh7712/algorithm/NumberLessThanX.java
@@ -0,0 +1,22 @@
+package lyh7712.algorithm;
+
+import java.util.Scanner;
+
+public class NumberLessThanX {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; ++i) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a.length; ++i) {
+            if (a[i] < x) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
